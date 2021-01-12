a34e194a9adae7aa7b8cdde8978177e3251d512e
robertlato <latoszewskirobert@gmail.com>
2021-01-12 21:20:06 +0100

    Add service annotation for BookCopyService

--- Booksharing/src/main/java/pl/edu/pg/booksharing/Booksharing/service/BookCopyServiceImpl.java
+++ Booksharing/src/main/java/pl/edu/pg/booksharing/Booksharing/service/BookCopyServiceImpl.java
@@ -1,11 +1,13 @@
 package pl.edu.pg.booksharing.Booksharing.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pl.edu.pg.booksharing.Booksharing.model.BookCopy;
 import pl.edu.pg.booksharing.Booksharing.repository.BookCopyRepository;
 
 import java.util.List;
 
+@Service
 public class BookCopyServiceImpl implements BookCopyService {
 
     @Autowired
