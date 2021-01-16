a2e1c3290ad1f35816e2fc504e8646ddd357f4a1
fmeina <filipmeina@gmail.com>
2021-01-16 11:12:30 +0100

    Added authentication

---
+++ Booksharing/src/main/java/pl/edu/pg/booksharing/Booksharing/config/AuthenticationBean.java
@@ -0,0 +1,20 @@
+package pl.edu.pg.booksharing.Booksharing.config;
+
+public class AuthenticationBean {
+
+    private String message;
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
