d270bc4ce61b92a605933cc0fb70a623e8592f16
fmeina <filipmeina@gmail.com>
2021-01-25 19:35:33 +0100

    Added WebConfig

---
+++ Booksharing/src/main/java/pl/edu/pg/booksharing/Booksharing/config/WebConfig.java
@@ -0,0 +1,15 @@
+package pl.edu.pg.booksharing.Booksharing.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
+    }
+}
