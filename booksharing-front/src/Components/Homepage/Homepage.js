7170dabf65fab285673551e42258b04dd0eb797c
filipqnicki <filipqnicki@gmail.com>
2021-01-28 23:08:27 +0100

    dodanie dodawanie książki przez użytkownika do swojej biblioteki

--- booksharing-front/src/Components/Homepage/Homepage.js
+++ booksharing-front/src/Components/Homepage/Homepage.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Homepage.css";
 import MyMap from "../MyMap/MyMap";
+import AuthenticationService from "../../service/AuthenticationService";
 
 class Homepage extends React.Component {
     render() {
+        const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
         return (
             <div>
                 <h1>Homepage</h1>
+                {isUserLoggedIn ? (
+                    <div> JEST ZALOGOWANY </div>
+                ) : (
+                    <div>NIE JEST ZALOGOWANY</div>
+                )}
                 <MyMap />
             </div>
         );
