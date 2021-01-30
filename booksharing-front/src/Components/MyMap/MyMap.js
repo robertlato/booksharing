e59c5259d5b245ac08b8bbe5373f76f0e7d0ba6b
jadaamska <37535325+jadaamska@users.noreply.github.com>
2021-01-30 08:47:42 +0100

    google maps,geocoding

--- booksharing-front/src/Components/MyMap/MyMap.js
+++ booksharing-front/src/Components/MyMap/MyMap.js
@@ -1,43 +1,100 @@
 //externals
-import React from "react";
+//api-key AIzaSyC06P_1LI2SP5bz3D_iUxgiBPzD5xpkUt8
+import React, { Component } from 'react';
+import axios from "axios";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import "./MyMap.css";
 
-//open layers and styles
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import XYZ from "ol/source/XYZ";
+const API_URL = "http://localhost:8889";
 
-class MyMap extends React.Component {
-    constructor(props) {
-        super(props);
+const mapStyles = {
+  width: '60%',
+  height: '60%'
+};
+var formattedAddress;
+geocode();
 
-        this.state = { center: [0, 0], zoom: 2 };
+function geocode(){
+    const location = 'Zielony Trójkąt 4 80-869 Gdańsk';
+    //const location = `${API_URL}/sharepoint/address`;
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+            address: location,
+            key: 'AIzaSyC06P_1LI2SP5bz3D_iUxgiBPzD5xpkUt8'
+        }
+    }).then(function(response){
+        console.log(response);
 
-        this.map = new Map({
-            target: null,
-            layers: [
-                new TileLayer({
-                    source: new XYZ({
-                        url:
-                            "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    }),
-                }),
-            ],
-            view: new View({
-                center: this.state.center,
-                zoom: this.state.zoom,
-            }),
-        });
-    }
+        formattedAddress = response.data.results[0].formatted_address;
+        var latAddress = response.data.results[0].geometry.location.lat;
+        var lngAddress = response.data.results[0].geometry.location.lng;
 
-    componentDidMount() {
-        this.map.setTarget("map");
-    }
-
-    render() {
-        return <div id="map"></div>;
-    }
+        console.log(formattedAddress);
+        console.log(latAddress);
+        console.log(lngAddress);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
 }
 
-export default MyMap;
+export class MyMap extends Component {
+    state = {
+        showingInfoWindow: false,  
+        activeMarker: {},          
+        selectedPlace: {}        
+      };
+
+    onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={12}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 54.352229015069526,
+            lng: 18.646201672142887
+          }
+        }
+      > 
+       <Marker
+          onClick={this.onMarkerClick}
+          position={{ lat: 54.3780294, lng: 18.6290507 }} // tutaj dac z bazy adresy
+          name={formattedAddress} // dac z bazy nazwe w dymku
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyC06P_1LI2SP5bz3D_iUxgiBPzD5xpkUt8'
+})(MyMap);
+
+
